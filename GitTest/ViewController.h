f2f57da654467c677a673ff5d47e55d64dd579ff
superbobogithub <1114026801@qq.com>
2015-10-29 10:26:48 +0800

    bobo initial commit

---
+++ GitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by SGB on 15/10/29.
+//  Copyright © 2015年 SGB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitTest
+//
+//  Created by SGB on 15/10/29.
+//  Copyright © 2015年 SGB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
